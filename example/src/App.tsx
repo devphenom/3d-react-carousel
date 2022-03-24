b83ec143d54f23856be59b9d59833a952438e329
devpenom <iphenom01@gmail.com>
2022-03-24 17:38:50 +0100

    Version One

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,19 +1,22 @@
 import React from 'react'
 
+// import Carousel from './Carousel'
 import { Carousel } from '3d-react-carousel'
 import '3d-react-carousel/dist/index.css'
 
 const App = () => {
   return (
-    <Carousel>
-      <img src='https://picsum.photos/800/304/?random' alt='5' />
-      <img src='https://picsum.photos/800/304/?random' alt='5' />
-      <img src='https://picsum.photos/800/304/?random' alt='5' />
-      <img src='https://picsum.photos/800/304/?random' alt='5' />
-      <img src='https://picsum.photos/800/304/?random' alt='5' />
-      <img src='https://picsum.photos/800/304/?random' alt='5' />
-      <img src='https://picsum.photos/800/304/?random' alt='5' />
-    </Carousel>
+    <div className='container'>
+      <Carousel>
+        <img src='https://picsum.photos/800/304/?random' alt='5' />
+        <img src='https://picsum.photos/801/304/?random' alt='5' />
+        <img src='https://picsum.photos/802/304/?random' alt='5' />
+        <img src='https://picsum.photos/803/304/?random' alt='5' />
+        <img src='https://picsum.photos/805/304/?random' alt='5' />
+        <img src='https://picsum.photos/806/304/?random' alt='5' />
+        <img src='https://picsum.photos/807/304/?random' alt='5' />
+      </Carousel>
+    </div>
   )
 }
 
--- example/src/Carousel.tsx
+++ example/src/Carousel.tsx
@@ -1,5 +1,5 @@
 import React, { Children, useEffect, useRef, useState } from 'react'
-import styles from './styles.module.css'
+// import styles from './index.module.css'
 
 type Carousel3dProps = {
   children: React.ReactNode
@@ -42,29 +42,29 @@ const Carousel = ({ children, direction = 'ltr' }: Carousel3dProps) => {
   }, [])
 
   return (
-    <div className={styles.carousel3d}>
-      <section className={`${styles.slider} ${styles[`${direction}`]}`}>
+    <div className={'carousel3d'}>
+      <section className={`slider ${direction}`}>
         {React.Children.map(arrayChildren, (child: React.ReactNode, index) => (
           <div
             key={`card-${index + 1}`}
-            className={`${styles.cSlide} ${
+            className={`${'cSlide'} ${
               (labelIndex === 1 && index + 1 === arrayChildren.length) ||
               (labelIndex > 1 && labelIndex - 2 === index)
-                ? styles.cSlidePrev
+                ? 'cSlidePrev'
                 : (labelIndex === 1 && index + 2 === arrayChildren.length) ||
                   (labelIndex === 2 && index + 1 === arrayChildren.length) ||
                   (labelIndex > 1 && labelIndex - 3 === index)
-                ? styles.cSlidePrev2
+                ? 'cSlidePrev2'
                 : (labelIndex === arrayChildren.length - 1 && index === 0) ||
                   labelIndex + 2 === index + 1 ||
                   (labelIndex === arrayChildren.length - 1 && index === 0) ||
                   (labelIndex === arrayChildren.length && index === 1)
-                ? styles.cSlideNext2
+                ? 'cSlideNext2'
                 : labelIndex === index + 1
-                ? styles.cSlideActive
+                ? 'cSlideActive'
                 : labelIndex + 1 === index + 1 ||
                   (labelIndex === arrayChildren.length && index === 0)
-                ? styles.cSlideNext
+                ? 'cSlideNext'
                 : ''
             }`}
           >
