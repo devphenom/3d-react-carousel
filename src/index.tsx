1d19276c12856ef22d84a00ca598e17241c83dea
Lateef Quadri <lateef9816@gmail.com>
2023-05-15 14:51:31 +0100

    Fix: direction prop

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,13 @@
 import * as React from 'react'
-import Carousel3d from './Carousel3d'
+import Carousel3d, { CarouselDirectionType } from './Carousel3d'
 
 // import styles from './styles.module.css'
 
 interface Props {
   children: React.ReactNode
+  direction?: CarouselDirectionType
 }
 
-export const Carousel = ({ children }: Props) => {
-  return <Carousel3d>{children}</Carousel3d>
+export const Carousel = ({ children, direction }: Props) => {
+  return <Carousel3d direction={direction}>{children}</Carousel3d>
 }
